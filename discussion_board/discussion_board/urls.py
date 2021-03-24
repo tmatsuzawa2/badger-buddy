8b4425d59f23b4e4760a21f3922e0ac8d7e577bf
Bryan <bjin23@wisc.edu>
2021-03-24 14:48:21 -0500

    began adding create-reply page

--- discussion_board/discussion_board/urls.py
+++ discussion_board/discussion_board/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('create-post', views.create_post, name='create-post'),
+    path('create-reply', views.create_reply, name='create-reply'),
 ]
