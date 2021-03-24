c63008bf34c564cfbe858e5d5bf7b8c4722bb2fe
Bryan <bjin23@wisc.edu>
2021-03-24 16:31:05 -0500

    create reply form, show comments below posts

--- discussion_board/discussion_board/urls.py
+++ discussion_board/discussion_board/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+# app_name = 'discussion_board'
 urlpatterns = [
     path('', views.index, name='index'),
     path('create-post', views.create_post, name='create-post'),
-    path('create-reply', views.create_reply, name='create-reply'),
+    path('create-reply/<int:post_id>', views.create_reply, name='create-reply'),
 ]
