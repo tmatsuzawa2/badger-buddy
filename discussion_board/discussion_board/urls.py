04d256d0e79e1c50290e4c23ac8c8f2553695907
Archana90663 <adhyani@wisc.edu>
2021-04-05 22:12:53 -0500

    Enabled to view entire post and replies when clicked

--- discussion_board/discussion_board/urls.py
+++ discussion_board/discussion_board/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='board_index'),
     path('create-post', views.create_post, name='create-post'),
     path('create-reply/<int:post_id>', views.create_reply, name='create-reply'),
+    path('view-post/<int:post_id>', views.view_post, name='view-post'),
 ]
