0220f48a5e0d919d317cf2c24a2f6cb10d6d29c1
jthal <jthalacker@wisc.edu>
2021-04-07 18:13:24 -0500

    rebasing commit

--- discussion_board/help/views.py
+++ discussion_board/help/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponseRedirect
+import random
+from ..models import Quotes
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 def index(request):
+    quote = get_object_or_404(Quotes, id=random.randrange(2, Quotes.objects.all().count()))
     context = {
+        'quote': quote
     }
     return render(request, 'help_resources/index.html', context)
