f6123037c53653e64a525b45accb6d668abc0119
tmatsuzawa2 <tmatsuzawa@wisc.edu>
2021-04-27 15:57:30 -0500

    Added test for 404 page

--- discussion_board/tests.py
+++ discussion_board/tests.py
@@ -154,7 +154,17 @@ class PostTests(TestCase):
         # Check if the html response contains the post
         self.assertTrue("sometitle" in response.content.decode("utf-8"))
 
+    def test_anonymous_own_post(self):
+        # Create reply
+        response = self.client.post("/board/create-post", {'title': 'sometitle', 'details': 'somedetails', 'anonymous': 'True'})
+        # View the post
+        response = self.client.get("/board/view-post/2")
+        # Check if the name is hidden
+        self.assertTrue("Anonymous to other students" in response.content.decode("utf-8"))
+
     def test_anonymous_post(self):
+        self.client.logout()
+        self.client.force_login(self.user2)
         # Create reply
         response = self.client.post("/board/create-post", {'title': 'sometitle', 'details': 'somedetails', 'anonymous': 'True'})
         # View the post
@@ -456,3 +466,11 @@ class HelpExerciseTest(TestCase):
         # If the author is null, then automatically set to anonymous
         response = self.client.post('/exercises/')
         self.assertTrue("-Anonymous" in response.content.decode("utf-8"))
+
+class Handle404Tests(TestCase):
+    def test_handler_renders_template_response(self):
+        response = self.client.get('/adsfa/')
+        # The 404 page was correctly handled
+        self.assertEqual(response.status_code, 200)
+        # Display the right page
+        self.assertTrue('404' in response.content.decode("utf-8"))
