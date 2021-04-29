650d12b05dcc1aebf701ca792e89756e72759572
tmatsuzawa2 <tmatsuzawa@wisc.edu>
2021-04-29 17:41:15 -0500

    turn the debug mode off for 404 pages

--- badger_buddy/settings.py
+++ badger_buddy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@qktco1#5--x=)=)golvow_sh6_r^80656p&6h4t-2_jevs=p2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', 'localhost', '127.0.0.1', '[::1]']
 
 # Login redirection
 LOGIN_REDIRECT_URL = '/board'
